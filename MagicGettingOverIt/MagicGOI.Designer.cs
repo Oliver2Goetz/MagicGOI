864620333e0c7219241ce06b21df8681736bda2c
Oliver Goetz <oli.goetz.o.g@gmail.com>
2022-11-25 12:50:31 +0100

    Project creation and finishing

---
+++ MagicGettingOverIt/MagicGOI.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace MagicGettingOverIt {
+    partial class MagicGOI {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureboxExit = new System.Windows.Forms.PictureBox();
+            this.textboxSavefileName = new System.Windows.Forms.TextBox();
+            this.buttonSavefileSave = new System.Windows.Forms.Button();
+            this.buttonSavefileLoad = new System.Windows.Forms.Button();
+            this.comboboxSaves = new System.Windows.Forms.ComboBox();
+            this.buttonSavefilesRefresh = new System.Windows.Forms.Button();
+            this.buttonSavefileDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelSavefilemanagement = new System.Windows.Forms.Label();
+            this.labelDivider = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureboxExit
+            // 
+            this.pictureboxExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureboxExit.Image = global::MagicGettingOverIt.Properties.Resources.cross;
+            this.pictureboxExit.Location = new System.Drawing.Point(548, 12);
+            this.pictureboxExit.Name = "pictureboxExit";
+            this.pictureboxExit.Size = new System.Drawing.Size(40, 40);
+            this.pictureboxExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureboxExit.TabIndex = 0;
+            this.pictureboxExit.TabStop = false;
+            this.pictureboxExit.Click += new System.EventHandler(this.pictureboxExit_Click);
+            // 
+            // textboxSavefileName
+            // 
+            this.textboxSavefileName.Location = new System.Drawing.Point(142, 154);
+            this.textboxSavefileName.Name = "textboxSavefileName";
+            this.textboxSavefileName.Size = new System.Drawing.Size(190, 23);
+            this.textboxSavefileName.TabIndex = 1;
+            // 
+            // buttonSavefileSave
+            // 
+            this.buttonSavefileSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonSavefileSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSavefileSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSavefileSave.Location = new System.Drawing.Point(12, 150);
+            this.buttonSavefileSave.Name = "buttonSavefileSave";
+            this.buttonSavefileSave.Size = new System.Drawing.Size(114, 30);
+            this.buttonSavefileSave.TabIndex = 2;
+            this.buttonSavefileSave.TabStop = false;
+            this.buttonSavefileSave.Text = "Save";
+            this.buttonSavefileSave.UseVisualStyleBackColor = true;
+            this.buttonSavefileSave.Click += new System.EventHandler(this.buttonSavefileSave_Click);
+            // 
+            // buttonSavefileLoad
+            // 
+            this.buttonSavefileLoad.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonSavefileLoad.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSavefileLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSavefileLoad.Location = new System.Drawing.Point(12, 114);
+            this.buttonSavefileLoad.Name = "buttonSavefileLoad";
+            this.buttonSavefileLoad.Size = new System.Drawing.Size(114, 30);
+            this.buttonSavefileLoad.TabIndex = 3;
+            this.buttonSavefileLoad.TabStop = false;
+            this.buttonSavefileLoad.Text = "Load";
+            this.buttonSavefileLoad.UseVisualStyleBackColor = true;
+            this.buttonSavefileLoad.Click += new System.EventHandler(this.buttonSavefileLoad_Click);
+            // 
+            // comboboxSaves
+            // 
+            this.comboboxSaves.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboboxSaves.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboboxSaves.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboboxSaves.FormattingEnabled = true;
+            this.comboboxSaves.Location = new System.Drawing.Point(142, 118);
+            this.comboboxSaves.Name = "comboboxSaves";
+            this.comboboxSaves.Size = new System.Drawing.Size(190, 24);
+            this.comboboxSaves.TabIndex = 4;
+            // 
+            // buttonSavefilesRefresh
+            // 
+            this.buttonSavefilesRefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonSavefilesRefresh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSavefilesRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSavefilesRefresh.Location = new System.Drawing.Point(347, 114);
+            this.buttonSavefilesRefresh.Name = "buttonSavefilesRefresh";
+            this.buttonSavefilesRefresh.Size = new System.Drawing.Size(114, 30);
+            this.buttonSavefilesRefresh.TabIndex = 5;
+            this.buttonSavefilesRefresh.TabStop = false;
+            this.buttonSavefilesRefresh.Text = "Refresh";
+            this.buttonSavefilesRefresh.UseVisualStyleBackColor = true;
+            this.buttonSavefilesRefresh.Click += new System.EventHandler(this.buttonSavefilesRefresh_Click);
+            // 
+            // buttonSavefileDelete
+            // 
+            this.buttonSavefileDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonSavefileDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSavefileDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSavefileDelete.Location = new System.Drawing.Point(474, 114);
+            this.buttonSavefileDelete.Name = "buttonSavefileDelete";
+            this.buttonSavefileDelete.Size = new System.Drawing.Size(114, 30);
+            this.buttonSavefileDelete.TabIndex = 6;
+            this.buttonSavefileDelete.TabStop = false;
+            this.buttonSavefileDelete.Text = "Delete";
+            this.buttonSavefileDelete.UseVisualStyleBackColor = true;
+            this.buttonSavefileDelete.Click += new System.EventHandler(this.buttonSavefileDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 37);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "MagicGOI - Getting Over It";
+            // 
+            // labelSavefilemanagement
+            // 
+            this.labelSavefilemanagement.AutoSize = true;
+            this.labelSavefilemanagement.Location = new System.Drawing.Point(221, 82);
+            this.labelSavefilemanagement.Name = "labelSavefilemanagement";
+            this.labelSavefilemanagement.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.labelSavefilemanagement.Size = new System.Drawing.Size(145, 17);
+            this.labelSavefilemanagement.TabIndex = 9;
+            this.labelSavefilemanagement.Text = "savefile management";
+            // 
+            // labelDivider
+            // 
+            this.labelDivider.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelDivider.Location = new System.Drawing.Point(12, 91);
+            this.labelDivider.Name = "labelDivider";
+            this.labelDivider.Size = new System.Drawing.Size(576, 2);
+            this.labelDivider.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 205);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.label2.Size = new System.Drawing.Size(508, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Notice: Go back to the main menu and then load the save in order for it to work";
+            // 
+            // MagicGOI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(600, 250);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelSavefilemanagement);
+            this.Controls.Add(this.labelDivider);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSavefileDelete);
+            this.Controls.Add(this.buttonSavefilesRefresh);
+            this.Controls.Add(this.comboboxSaves);
+            this.Controls.Add(this.buttonSavefileLoad);
+            this.Controls.Add(this.buttonSavefileSave);
+            this.Controls.Add(this.textboxSavefileName);
+            this.Controls.Add(this.pictureboxExit);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MagicGOI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MagicGettingOverIt";
+            this.Load += new System.EventHandler(this.MagicGettingOverIt_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MagicCSGO_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxExit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureboxExit;
+        private System.Windows.Forms.TextBox textboxSavefileName;
+        private System.Windows.Forms.Button buttonSavefileSave;
+        private System.Windows.Forms.Button buttonSavefileLoad;
+        private System.Windows.Forms.ComboBox comboboxSaves;
+        private System.Windows.Forms.Button buttonSavefilesRefresh;
+        private System.Windows.Forms.Button buttonSavefileDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelSavefilemanagement;
+        private System.Windows.Forms.Label labelDivider;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MagicGettingOverIt/MagicGOI.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.Win32;
+
+namespace MagicGettingOverIt {
+
+    public partial class MagicGOI : Form {
+
+        private static String magicRoot = "";
+        private static String savefileDirectory = "";
+
+        private static String registryPath = @"HKEY_CURRENT_USER\SOFTWARE\Bennett Foddy\Getting Over It";
+
+        public MagicGOI() {
+            InitializeComponent();
+        }
+
+        private void MagicGettingOverIt_Load(object sender, EventArgs e) {
+            this.setMagicRoot();
+            this.initializeSavefileDirectory();
+            this.refreshSavefiles();
+        }
+
+        private void setMagicRoot() {
+            magicRoot = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\MagicGettingOverIt";
+        }
+
+        private void initializeSavefileDirectory() {
+            if(!Directory.Exists(magicRoot)) {
+                Directory.CreateDirectory(magicRoot);
+            }
+
+            savefileDirectory = magicRoot + "\\" + "savefiles";
+            if(!Directory.Exists(savefileDirectory)) {
+                Directory.CreateDirectory(savefileDirectory);
+            }
+        }
+
+        #region form
+
+        // Make window moveable
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void MagicCSGO_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        /*
+         * Exit form on click
+         */
+        private void pictureboxExit_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        #endregion form
+
+        #region savefile management
+
+        private void buttonSavefileLoad_Click(object sender, EventArgs e) {
+            if (comboboxSaves.SelectedIndex != -1) {
+                string selected = (string)comboboxSaves.SelectedItem;
+
+                if (0 < selected.Length) {
+                    try {
+                        string filename0 = selected + "_0.xml";
+                        string filename1 = selected + "_1.xml";
+
+                        byte[] savefileContent0 = File.ReadAllBytes(savefileDirectory + "\\" + filename0);
+                        byte[] savefileContent1 = File.ReadAllBytes(savefileDirectory + "\\" + filename1);
+
+                        if (0 < savefileContent0.Length && 0 < savefileContent1.Length) {
+                            this.changeRegistryEntry("SaveGame0_h1867918426", savefileContent0);
+                            this.changeRegistryEntry("SaveGame1_h1867918427", savefileContent1);
+                        }
+                    } catch (Exception ex) {
+                        MessageBox.Show(ex.Message, "Error loading savefile.", MessageBoxButtons.OK);
+                    }
+                }
+            }
+        }
+
+        private void buttonSavefileSave_Click(object sender, EventArgs e) {
+            string savefileName = textboxSavefileName.Text;
+
+            if(0 < savefileName.Length) {
+                try {
+                    String newSavefilePath0 = savefileName + "_0.xml";
+                    String newSavefilePath1 = savefileName + "_1.xml";
+
+                    byte[] bytesFile0 = this.readFromRegistry("SaveGame0_h1867918426");
+                    byte[] bytesFile1 = this.readFromRegistry("SaveGame1_h1867918427");
+
+                    File.WriteAllBytes(savefileDirectory + "\\" + newSavefilePath0, bytesFile0);
+                    File.WriteAllBytes(savefileDirectory + "\\" + newSavefilePath1, bytesFile1);
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message, "Error saving savefile.", MessageBoxButtons.OK);
+                }
+            }
+
+            refreshSavefiles();
+        }
+
+        private void buttonSavefilesRefresh_Click(object sender, EventArgs e) {
+            this.refreshSavefiles();
+        }
+
+        private void buttonSavefileDelete_Click(object sender, EventArgs e) {
+            string selected = (string)comboboxSaves.SelectedItem;
+
+            string filename0 = selected + "_0.xml";
+            string filename1 = selected + "_1.xml";
+
+            try {
+                File.Delete(savefileDirectory + "\\" + filename0);
+                File.Delete(savefileDirectory + "\\" + filename1);
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error deleting savefile.", MessageBoxButtons.OK);
+            }
+
+            this.refreshSavefiles();
+        }
+
+        /*
+         * Refreshs the list of savefiles in the combobox
+         */
+        private void refreshSavefiles() {
+            string[] savefiles = Directory.GetFiles(savefileDirectory);
+
+            comboboxSaves.SelectedIndex = -1;
+            comboboxSaves.Items.Clear();
+            foreach (string savefile in savefiles) {
+                string basename = Path.GetFileName(savefile);
+                string strippedBasename = basename.Replace("_0.xml", "").Replace("_1.xml", "");
+                if (!comboboxSaves.Items.Contains(strippedBasename)) {
+                    comboboxSaves.Items.Add(strippedBasename);
+                }
+            }
+        }
+
+        #endregion savefile management
+
+        #region registry handling
+
+        /*
+         * Reads the value from the windows registry for the given path and key
+         */
+        public byte[] readFromRegistry(String key) {
+            return (byte[])Registry.GetValue(registryPath, key, new object());
+        }
+
+        /*
+         * Changes a value in the windows registry for the given path and key
+         */
+        public void changeRegistryEntry(String key, byte[] value) {
+            Registry.SetValue(registryPath, key, value, RegistryValueKind.Binary);
+        }
+
+        #endregion registry handling
+
+    }
+}
